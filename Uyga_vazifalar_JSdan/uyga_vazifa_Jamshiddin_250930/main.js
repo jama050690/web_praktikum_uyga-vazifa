e5abba8c6dc07586d69cfb0272066856ad8a6f21
Jamshiddin <jbm050690@gmail.com>
2025-10-01 08:43:02 +0300

    add

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_250930/main.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_250930/main.js
@@ -5,13 +5,41 @@ const prompt = require("prompt-sync")();
 function masala_1() {
   console.log("Masala 1...");
   let k = Number(prompt("Time(second) creating"));
-  a = Math.abs(n);
-   const=0;
-for (let i=0;i<k;i++)
+  let n = Number(prompt("Time(second) creating"));
+
+  k = Math.abs(k);
+  for (let i = 1; i <= n; i++) {
+    if (n > 0) {
+      console.log(k);
+    } else {
+      console.log(n > 0, "ni kiriting");
+    }
+  }
 }
-// Masalalarni ishga tushurish qismi
+
+// a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b ni ham) chiqaruvchi va
+// chiqarilgan sonlar sonini chiqaruvchi programma tuzilsin. (a va b xam chiqarilsin).
+
+const prompt = require("prompt-sync")();
+
+function masala_2() {
+  console.log("Masala 2...");
+  let a = Number(prompt("Time(second) creating"));
+  let b = Number(prompt("Time(second) creating"));
+
+  k = Math.abs(k);
+  for (let i = 1; i <= k; i++) {
+    if (b > a) {
+      console.log(k);
+    } else {
+      console.log(b > a, "ni kiriting");
+    }
+  }
+}
+
 let displayCommands;
 "----=== MASALA RAQAMLARI ===----\n" + "1-masala - 1\n";
+"1-masala - 1\n" + "2-masala - 2\n";
 
 console.log(displayCommands);
 
@@ -21,6 +49,9 @@ let num = Number(prompt());
 switch (num) {
   case 1:
     masala_1();
+    break;
+  case 2:
+    masala_2();
     break;
 
     break;
