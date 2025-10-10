6be8172631f2a30870ef82cafbf8e403c21d2f34
Jamshiddin <jbm050690@gmail.com>
2025-10-10 09:25:14 +0500

    add

--- Uyga_vazifalar_JSdan/uyga_vazifa_js_Jamshiddin_qo'shimcha_amaliyot_251009/masala_1.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_js_Jamshiddin_qo'shimcha_amaliyot_251009/masala_1.js
@@ -1,5 +1,6 @@
 const prompt = require("prompt-sync")();
-// misol_3:
+
+// 1-topshiriq.
 // Berilgan sonlar massividan faqat juft sonlarni toping va har birini kvadratga ko'taring.
 // Natijalarni yangi massivda qaytaring.
 // let arr= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]     // Natija : [4, 16, 36, 64, 100]
@@ -7,12 +8,10 @@ const prompt = require("prompt-sync")();
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let arrayNatija = [];
-let j = 0;
 const arrayJuft = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
       arrayNatija[j] = arr[i] ** 2;
-      j++;
     }
   }
   return arrayNatija;
---
+++ Uyga_vazifalar_JSdan/uyga_vazifa_js_Jamshiddin_qo'shimcha_amaliyot_251009/masala_2.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")();
+
+// 2-topshiriq.
+// Berilgan sonlar massividagi elementlarni o'sish tartibida (kichikdan kattaga) tartiblang.
+// let arr= [2, 5, 10, 7, 4, 6, 1, 0, 10, 8]   // Natija : [ 1, 2, 4,  5, 6, 7, 8, 10 ]
+
+let arr = [2, 5, 10, 7, 4, 6, 1, 0, 10, 8];
+const arrayJuft = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+};
+console.log(arr);
