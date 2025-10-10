baf6dea1cc314e0992cc4c2a5985926f29a03e82
Jamshiddin <jbm050690@gmail.com>
2025-10-10 11:29:59 +0500

    add 11

---
+++ Uyga_vazifalar_JSdan/uyga_vazifa_js_Jamshiddin_qo'shimcha_amaliyot_251009/masala_10.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+// 11-topshiriq
+// array ni oxiriga yangi index qo'shish(Push);
+let arr = [1, 2, 3, 5, 6];
+function myPush(array, element) {
+  array[array.lenght] = element;
+  return array;
+}
+let result = myPush(arr, 15);
+console.log(result);
