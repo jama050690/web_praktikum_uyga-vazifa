f476910143a31f84f417592e1d7d5fba53d362e3
Jamshiddin <jbm050690@gmail.com>
2025-10-20 10:25:36 +0500

    add homework array wiht contact moodel js

---
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251018/main.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+
+// masala-1:
+// n natural soni berilgan. Dastlabki n ta toq sondan tashkil topgan
+// massivni hosil qiling va elementlarini chiqaring.
+
+let n = Number(prompt("Natural son kiriting: "));
+
+const arToqson = (son) => {
+  let arr = [];
+  for (let i = 1; arr.length < son; i += 2) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+let result = arToqson(n);
+console.log("Dastlabki " + n + " ta toq son:", result);
