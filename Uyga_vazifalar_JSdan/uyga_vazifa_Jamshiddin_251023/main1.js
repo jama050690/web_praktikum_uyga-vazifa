0e41427718974550424f26706fbbf046737b292d
Jamshiddin <jbm050690@gmail.com>
2025-10-25 10:42:23 +0500

    add

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251023/main1.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251023/main1.js
@@ -30,5 +30,33 @@ class Product {
     return `Mahsulot: ${this.name}\nID: ${this.id}\nNarx: ${this.price}\nMiqdor: ${this.quantity}`;
   }
 }
-const newProduct = new Product(1234, "iPhone", 2500000, 100 + "ta");
-console.log(newProduct);
+class Customer {
+  constructor(name, phone, balance = 0, purchase_history = []) {
+    this.name = name;
+    this.phone = phone;
+    this.balanse = balance;
+    this.purchase_history = purchase_history;
+  }
+  add_money(amount) {
+    this.balanse += amount;
+    return ` Balans yangilandi: ${this.balanse} so'm`;
+  }
+  show_balance() {
+    return ` Mijsozda: ${this.balanse} so'm mavjud`;
+  }
+  show_history() {
+    return this.purchase_history.length === 0
+      ? "Xarid mavjud emas"
+      : this.purchase_history.join(", ");
+  }
+}
+
+// const newProduct = new Product(1234, "iPhone", 2500000, 100 + "ta");
+// console.log(newProduct);
+
+// const customer1 = new Customer("Javlon", "+998901234567", 100000);
+
+// console.log(customer1.show_balance());
+// console.log(customer1.add_money(50000));
+// console.log(customer1.show_balance());
+// console.log(customer1.show_history());
