f81e40992d7c1816ef92cc17a2d901b78dcd757e
Jamshiddin <jbm050690@gmail.com>
2025-10-26 20:37:08 +0500

    add

---
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/class_masalalar/main.js
@@ -0,0 +1,21 @@
+// Topshiriq -  1: Talaba Klassi ( oson )
+
+// Talaba haqida ma'lumot saqlaydigan klass yarating.
+// Constructor yarating (name, age, course)
+// introduce() method - "Mening ismim Ali, men 20 yoshdaman"
+// study() method - "Ali Frontend kursida dars o'qiyapti"
+// 2 ta talaba yarating va ularning methodlarini chaqiring
+
+class Student {
+  constructor(name, age, course) {
+    (this.name = name), (this.name = age), (this.name = course);
+  }
+  introduce() {
+    return `Mening ismim ${this.name},men ${this.age} yoshdaman`;
+  }
+  study() {
+    return `${this.name} ${this.course} kusrsida o'qiyapti`;
+  }
+}
+let talaba = new Student("Ali", 20, "Frontend");
+console.log(talaba);
