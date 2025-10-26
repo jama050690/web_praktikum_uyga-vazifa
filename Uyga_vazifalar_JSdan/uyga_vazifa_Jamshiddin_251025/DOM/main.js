37c34029a205e17c803f4b21750757091f6fd0d7
Jamshiddin <jbm050690@gmail.com>
2025-10-26 20:17:06 +0500

    add

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/DOM/main.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/DOM/main.js
@@ -2,3 +2,20 @@ const numberEl = document.getElementById("number");
 const subBtnEl = document.getElementById("sub-btn");
 const resetBtnEl = document.getElementById("reset-btn");
 const addBtnEl = document.getElementById("add-btn");
+
+subBtnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  let result = Number(numberEl.innerText);
+  numberEl.innerText = result - 1;
+});
+
+resetBtnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  numberEl.innerText = 0;
+});
+
+addBtnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  let result = Number(numberEl.innerText);
+  numberEl.innerText = result + 1;
+});
