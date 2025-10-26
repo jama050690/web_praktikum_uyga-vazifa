12e33ae8bfa8e2068a784c033d66465f17294631
Jamshiddin <jbm050690@gmail.com>
2025-10-26 21:29:19 +0500

    add

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/class_masalalar/main.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/class_masalalar/main.js
@@ -6,6 +6,8 @@
 // study() method - "Ali Frontend kursida dars o'qiyapti"
 // 2 ta talaba yarating va ularning methodlarini chaqiring
 
+console.log("Topshiriq-1");
+
 class Student {
   constructor(name, age, course) {
     this.name = name;
@@ -26,3 +28,41 @@ console.log(talaba.introduce());
 console.log(talaba.study());
 console.log(talaba_2.introduce());
 console.log(talaba_2.study());
+
+console.log("Topshiriq-2");
+
+// Topshiriq - 2: Kitob Klassi ( oson )
+// Kitob haqida ma'lumot saqlaydigan klass.
+// Kitob ma'lumotlarini saqlang
+// getInfo() - "Sarlavha: ..., Muallif: ..., Sahifalar: ..."
+// markAsRead() - isRead ni true ga o'zgartirsin
+// 3 ta kitob yarating va barcha methodlarni sinab ko'ring
+
+class Kitob {
+  constructor(sarlavha, muallif, sahifalar) {
+    this.sarlavha = sarlavha;
+    this.muallif = muallif;
+    this.sahifalar = sahifalar;
+    this.isRead = false;
+  }
+  getInfo() {
+    return `Sarlovha: ${this.sarlavha}. Muallif: ${this.muallif}. Sahifalar: ${this.sahifalar}`;
+  }
+  markAsRead() {
+    this.isRead = true;
+  }
+}
+let kitob_1 = new Kitob("Ikki eshik orasida", "Abdulla Qahhor", 400);
+console.log(kitob_1.getInfo(), kitob_1.isRead ? "O'qilgan" : "O'qilmagan");
+kitob_1.markAsRead();
+console.log(kitob_1.isRead ? "O'qilgan" : "O'qilmagan");
+
+let kitob_2 = new Kitob("Navoiy", "Oybek", 398);
+console.log(kitob_2.getInfo(), kitob_2.isRead ? "O'qilgan" : "O'qilmagan");
+kitob_2.markAsRead();
+console.log(kitob_2.isRead ? "O'qilgan" : "O'qilmagan");
+
+let kitob_3 = new Kitob("UFQ", "Said Ahmad", 411);
+console.log(kitob_3.getInfo(), kitob_3.isRead ? "O'qilgan" : "O'qilmagan");
+kitob_3.markAsRead();
+console.log(kitob_3.isRead ? "O'qilgan" : "O'qilmagan");
