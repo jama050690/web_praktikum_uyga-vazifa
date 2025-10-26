d3e85218a2e039384bfb492bccf4b84336cd5800
Jamshiddin <jbm050690@gmail.com>
2025-10-26 12:31:05 +0500

    add

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251018/main.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251018/main.js
@@ -7,23 +7,26 @@ import arrTeskari from "./masala_5_array.js";
 
 const prompt = promptSync();
 
-let n = Number(prompt("Natural son kiriting: "));
-let n1 = Number(prompt("Natural son kiriting: "));
-let n2 = Number(prompt("n: "));
-let A = Number(prompt("A: "));
-let D = Number(prompt("D: "));
-let n3 = Number(prompt("Natural son kiriting: "));
-let N4 = Number(prompt("Massiv uzunligini kiriting: "));
+let n = Number(prompt("Masala 1. Natural son kiriting: "));
 let toq = arToqson(n);
-let daraja = arDarajason(n1);
+console.log(`${n} ta toq sonlar:`, toq);
+
+let n1 = Number(prompt("Masala 2.\n Natural son kiriting: "));
+let n2 = Number(prompt("n: "));
+console.log(`${n1} ta 2 ning darajalari:`, arDarajason(n1));
+
+let A = Number(prompt("Masala 3.\n A: "));
+let D = Number(prompt("D: "));
+console.log(arrArifmetik(n2, A, D));
+
+let n3 = Number(prompt("Masala 4.\n Natural son kiriting: "));
+console.log(`${n3} ta Fibonachchi sonlari:`, arrFibanaci(n3));
+
+let N4 = Number(prompt(" Masala 5.\n Massiv uzunligini kiriting: "));
 let array = [];
 for (let i = 0; i < N4; i++) {
   array.push(Number(prompt(`${i + 1}-elementni kiriting: `)));
 }
-console.log(`${n} ta toq sonlar:`, toq);
-console.log(`${n1} ta 2 ning darajalari:`, daraja);
-console.log(arrArifmetik(n2, A, D));
-console.log(`${n3} ta Fibonachchi sonlari:`, arrFibanaci(n3));
-
 console.log("Asl massiv:", array);
 console.log("Teskari tartibdagi massiv:", arrTeskari([...array]));
+//
