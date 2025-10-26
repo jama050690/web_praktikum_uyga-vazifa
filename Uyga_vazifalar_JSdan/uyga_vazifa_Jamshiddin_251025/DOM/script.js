f60ec4c409a6d3454b02ab6b57cdb179c7d964c0
Jamshiddin <jbm050690@gmail.com>
2025-10-26 16:49:26 +0500

    add

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/DOM/script.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/DOM/script.js
@@ -11,14 +11,22 @@ btnEl.addEventListener("click", (e) => {
   let age = yoshEl.value;
   let city = shaharEl.value.trim();
   let bio = bioEl.value.trim();
+  const profile = {
+    name: name,
+    age: age,
+    city: city,
+    bio: bio,
+  };
   if (name.length == 0 || Number(age) == 0 || city.length == 0) {
-    alert("Iltimos barcha ma`lumotlarni kiriting");
+    alert("Iltimos barcha ma'lumotlarni kiriting");
   } else {
     foydalanuvchi_profilEl.innerText = `
-    Foydalanuvchi profili\n
-    Ism: ${name}\n 
-    Yosh: ${age}\n
-    Shahar: ${city}\n
-    Bio: ${bio.length == 0 ? "Ma'lumot mavjud emas" : bio} `;
+    Foydalanuvchi profili
+    Ism: ${name} 
+    Yosh: ${age}
+    Shahar: ${city}
+    Bio: ${bio.length == 0 ? "Ma'lumot mavjud emas" : bio} 
+    `;
   }
+  console.log(profile);
 });
