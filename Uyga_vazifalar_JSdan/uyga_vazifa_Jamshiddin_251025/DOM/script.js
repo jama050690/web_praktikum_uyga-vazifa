205fa4b4cd2e6c84d306209413e7757d27107618
Jamshiddin <jbm050690@gmail.com>
2025-10-26 16:34:34 +0500

    add

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/DOM/script.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/DOM/script.js
@@ -1,23 +1,24 @@
 const ismEl = document.getElementById("ism");
 const yoshEl = document.getElementById("yosh");
-const shahalEl = document.getElementById("shahar");
+const shaharEl = document.getElementById("shahar");
 const bioEl = document.getElementById("bio");
 const btnEl = document.getElementById("btn");
 const foydalanuvchi_profilEl = document.getElementById("foydalanuvchi-profil");
 
-btnEl.addEventListener("click", (PointerEvent) => {
-  let name = ismEl.value;
+btnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  let name = ismEl.value.trim();
   let age = yoshEl.value;
-  let city = shahalEl.value;
-  let about = bioEl.value;
-  if (
-    name.trim().length == 0 ||
-    Number(age) == 0 ||
-    city.trim().length == 0 ||
-    about.trim().length == 0
-  ) {
+  let city = shaharEl.value.trim();
+  let bio = bioEl.value.trim();
+  if (name.length == 0 || Number(age) == 0 || city.length == 0) {
     alert("Iltimos barcha ma`lumotlarni kiriting");
   } else {
-    let result = foydalanuvchi_profilEl.value;
+    foydalanuvchi_profilEl.innerText = `
+    Foydalanuvchi profili\n
+    Ism: ${name}\n 
+    Yosh: ${age}\n
+    Shahar: ${city}\n
+    Bio: ${bio.length == 0 ? "Ma'lumot mavjud emas" : bio} `;
   }
 });
