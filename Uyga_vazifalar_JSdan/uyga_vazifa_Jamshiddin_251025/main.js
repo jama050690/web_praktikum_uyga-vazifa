d53689fb5f701ab8ee8cb1726f339c366db9904d
Jamshiddin <jbm050690@gmail.com>
2025-10-26 11:20:08 +0500

    add topshiriq-2

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/main.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/main.js
@@ -47,3 +47,61 @@ console.log("\nQolgan talabalar:");
 for (let [id, student] of students) {
   console.log(`ID: ${id}`, student);
 }
+
+//Topshiriq - 2:  Mahsulotlar Savdo Tizimi
+// Online do'kon uchun mahsulotlar Map ini yarating:
+// Kamida 5 ta mahsulot qo'shing
+// Key sifatida mahsulot kodi (masalan: "PROD001")
+// Value: {name, price, stock, category}
+// Barcha mahsulot nomlarini chiqaring (keys() dan foydalaning)
+// Jami mahsulotlar sonini toping (size)
+// Ma'lum bir mahsulot mavjudligini tekshiring (has())
+// Barcha mahsulotlar va narxlarini for loop bilan chiqaring
+
+let shop = new Map();
+
+shop.set("PROD001", {
+  name: "Notebook",
+  price: 135000 + "so'm",
+  stock: 10 + "ta",
+  category: "Macbook",
+});
+shop.set("PROD002", {
+  name: "Smartphone",
+  price: 95000 + "so'm",
+  stock: 8 + "ta",
+  category: "IPhone",
+});
+shop.set("PROD003", {
+  name: "Keyboard",
+  price: 100000 + "so'm",
+  stock: 11 + "ta",
+  category: "Accessories",
+});
+shop.set("PROD004", {
+  name: "HeadPhones",
+  price: 120000 + "so'm",
+  stock: 15 + "ta",
+  category: "Accessories",
+});
+shop.set("PROD005", {
+  name: "Mouse",
+  price: 80000 + "so'm",
+  stock: 12 + "ta",
+  category: "Accessories",
+});
+console.log("BArcha mahsulotlar nomlar(keys()yordamida): ");
+for (let key of shop.keys()) {
+  console.log(` ${key} ${shop.get(key).name}`);
+}
+
+console.log("Barcha mahsolot soni: ", shop.size);
+const checkCode = "PROD003";
+console.log(
+  ` ${checkCode} mavjudmi?,
+  ${shop.has(checkCode) ? "Ha, mavjud" : "Yo'q"}`
+);
+
+for (let key of shop.keys()) {
+  console.log(`${key} ${shop.get(key).name} ${shop.get(key).price}`);
+}
