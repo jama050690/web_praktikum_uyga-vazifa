a662ef1f9a6494bda999816f7bc8c49551221be0
Jamshiddin <jbm050690@gmail.com>
2025-10-26 19:03:14 +0500

    add

---
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251025/DOM/main.js
@@ -0,0 +1,4 @@
+const numberEl = document.getElementById("number");
+const subBtnEl = document.getElementById("sub-btn");
+const resetBtnEl = document.getElementById("reset-btn");
+const addBtnEl = document.getElementById("add-btn");
