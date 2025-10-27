9bbe45318ff0abfd2bdabfdaf5f0f9cc452e3e70
Jamshiddin <jbm050690@gmail.com>
2025-10-27 15:48:14 +0500

    add

--- Uyga_vazifalar_JSdan/uyga_vazifa qo'shimcha_Jamshiddin_251025/svitafor/script.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa qo'shimcha_Jamshiddin_251025/svitafor/script.js
@@ -2,20 +2,27 @@ const qizil = document.getElementById("qizil");
 const sariq = document.getElementById("sariq");
 const yashil = document.getElementById("yashil");
 
+function resetLights() {
+  qizil.style.background = "#111";
+  sariq.style.background = "#111";
+  yashil.style.background = "#111";
+}
+
 function svetofor() {
+  resetLights();
   qizil.style.background = "red";
   setTimeout(() => {
     qizil.style.background = "#111";
     sariq.style.background = "yellow";
-  }, 2000);
+  }, 25000);
   setTimeout(() => {
     sariq.style.background = "#111";
     yashil.style.background = "green";
-  }, 4000);
+  }, 30000);
   setTimeout(() => {
     yashil.style.background = "#111";
-  }, 6000);
+  }, 50000);
 }
 
 svetofor();
-setInterval(svetofor, 6000);
+setInterval(svetofor, 50000);
