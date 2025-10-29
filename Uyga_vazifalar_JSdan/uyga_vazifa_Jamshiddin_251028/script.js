a3c26b691ce99e5c929b9a4a6e97e2e2588bced1
Jamshiddin <jbm050690@gmail.com>
2025-10-29 21:57:42 +0500

    add

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251028/script.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251028/script.js
@@ -1,1 +1,63 @@
-const boxEl = document.querySelector("container");
+const list = document.getElementById("list");
+const addBtn = document.getElementById("addBtn");
+const delBtn = document.getElementById("delBtn");
+
+addBtn.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.textContent = "Yangi element " + Date.now();
+  list.appendChild(li);
+});
+
+delBtn.addEventListener("click", () => {
+  if (list.lastChild) {
+    list.lastChild.remove();
+  }
+});
+
+// const userInput = document.querySelector(".input");
+// const userForm = document.querySelector(".form");
+// const box = document.querySelector(".box");
+// // /////////////////////////////////////////////
+
+// let tasks = [];
+
+// const render = () => {
+//   box.innerHTML = "";
+
+//   for (let i = 0; i < tasks.length; i++) {
+//     let title = document.createElement("h1");
+//     title.textContent = tasks[i].title;
+//     // h1
+//     let btn = document.createElement("button");
+//     btn.textContent = "Delete";
+//     btn.className = "deleteBtn";
+//     btn.id = tasks[i].id;
+//     // btn
+//     let wrapper = document.createElement("div");
+//     wrapper.className = "block";
+//     wrapper.append(title, btn);
+//     // wrapper
+//     box.append(wrapper);
+//   }
+// };
+
+// userForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   tasks.push({ title: userInput.value, id: Date.now() });
+//   userInput.value = "";
+//   render();
+// });
+
+// box.addEventListener("click", (e) => {
+//   const el = e.target;
+//   if (el.className === "deleteBtn") {
+//     for (let i = 0; i < tasks.length; i++) {
+//       if (tasks[i].id == el.id) {
+//         tasks.splice(i, 1);
+//       }
+//     }
+//   }
+
+//   render();
+// });
