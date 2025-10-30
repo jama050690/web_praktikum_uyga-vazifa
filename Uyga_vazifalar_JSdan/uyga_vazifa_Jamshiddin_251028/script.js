c3cd2d03f256dc4a79edf92d15603acad4c0fca5
Jamshiddin <jbm050690@gmail.com>
2025-10-30 13:02:42 +0500

    addd

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251028/script.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251028/script.js
@@ -1,68 +1,42 @@
-const titleInput = document.getElementById("input_title");
+const textInput = document.getElementById("input_title");
 const list = document.getElementById("list");
 const addBtn = document.getElementById("addBtn");
-const delBtn = document.getElementById("delBtn");
 
 let tasks = [];
 
-const nowBet = () => {};
+function deleteTask(id) {
+  tasks = tasks.filter((task) => task.id !== id);
+  renderList();
+}
+
+const renderList = () => {
+  list.innerHTML = "";
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
+    li.className = "list_item";
+
+    const result = document.createElement("p");
+    result.textContent = task.title + " — Men CRUD ni o'rganyapman";
+    li.appendChild(result);
+
+    const btn = document.createElement("button");
+    btn.classList.add("delete-btn");
+    btn.textContent = "Delete";
+    btn.addEventListener("click", () => deleteTask(task.id));
+
+    li.appendChild(btn);
+    list.appendChild(li);
+  });
+};
 
 addBtn.addEventListener("click", () => {
-  const li = document.createElement("li");
-  li.textContent = "Yangi element " + Date.now();
-  list.appendChild(li);
-});
-
-delBtn.addEventListener("click", () => {
-  if (list.lastChild) {
-    list.lastChild.remove();
+  const title = textInput.value.trim();
+  if (title === "") {
+    alert("Iltimos, matn kiriting!");
+    return;
   }
+
+  tasks.push({ id: Date.now(), title });
+  renderList();
+  textInput.value = "";
 });
-
-// const userInput = document.querySelector(".input");
-// const userForm = document.querySelector(".form");
-// const box = document.querySelector(".box");
-// // /////////////////////////////////////////////
-
-// let tasks = [];
-
-// const render = () => {
-//   box.innerHTML = "";
-
-//   for (let i = 0; i < tasks.length; i++) {
-//     let title = document.createElement("h1");
-//     title.textContent = tasks[i].title;
-//     // h1
-//     let btn = document.createElement("button");
-//     btn.textContent = "Delete";
-//     btn.className = "deleteBtn";
-//     btn.id = tasks[i].id;
-//     // btn
-//     let wrapper = document.createElement("div");
-//     wrapper.className = "block";
-//     wrapper.append(title, btn);
-//     // wrapper
-//     box.append(wrapper);
-//   }
-// };
-
-// userForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   tasks.push({ title: userInput.value, id: Date.now() });
-//   userInput.value = "";
-//   render();
-// });
-
-// box.addEventListener("click", (e) => {
-//   const el = e.target;
-//   if (el.className === "deleteBtn") {
-//     for (let i = 0; i < tasks.length; i++) {
-//       if (tasks[i].id == el.id) {
-//         tasks.splice(i, 1);
-//       }
-//     }
-//   }
-
-//   render();
-// });
