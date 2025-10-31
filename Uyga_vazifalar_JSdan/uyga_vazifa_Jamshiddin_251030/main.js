3f165635c05acddcc0fbc66f271714d714e46d66
Jamshiddin <jbm050690@gmail.com>
2025-10-31 21:30:30 +0500

    ADD

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251030/main.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251030/main.js
@@ -1,13 +1,42 @@
-const form = document.querySelector(".form");
-const input = document.querySelector(".input");
-const box = document.querySelector(".box");
+const formEl = document.querySelector(".form");
+const inputEl = document.querySelector(".input");
+const boxEl = document.querySelector(".block");
 
 let inputList = [];
 
-const create = () => {
-  box.innerHTML = taskList.map(
-    (item) => `<p> ${text}</p> 
-  <button class="deleteBtn>Delete</button>
- <button class="deleteBtn>Delete</button> `
-  );
+const refreshUI = () => {
+  boxEl.innerHTML = inputList
+    .map(
+      (item) => `
+        <div class="item">
+          <p>${item.text}</p>
+          <button class="deleteBtn" id="${item.id}">Delete</button>
+          <button class="editBtn" id="${item.id}">Edit</button>
+        </div>
+      `
+    )
+    .join("");
 };
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  inputList.push({ text: inputEl.value, id: Date.now() });
+  inputEl.value = "";
+  refreshUI();
+});
+boxEl.addEventListener("click", (e) => {
+  const obj = e.target;
+  console.log(obj);
+
+  if (obj.className === "deleteBtn") {
+    inputList = inputList.filter((item) => item.id !== Number(obj.id));
+  }
+  if (obj.className === "editBtn") {
+    const newText = prompt("");
+    inputList = inputList.map((item) =>
+      item.id === Number(obj.id) ? { ...item, text: newText } : item
+    );
+  }
+
+  refreshUI();
+});
