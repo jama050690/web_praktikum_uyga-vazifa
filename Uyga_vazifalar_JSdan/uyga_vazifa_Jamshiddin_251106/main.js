06d020b953ffde95644fefeb847ff7449c326ace
Jamshiddin <jbm050690@gmail.com>
2025-11-07 23:43:57 +0500

    add

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251106/main.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251106/main.js
@@ -1,36 +1,32 @@
+const boxEl = document.querySelector(".box");
 const formEl = document.querySelector(".form_input_main");
 const inputEl = document.querySelectorAll(".add_input");
-const main = document.querySelector(".main");
 const modal = document.querySelector(".form_div");
 
 const url = "http://localhost:3600/todos";
 const render = (data) => {
-  console.log("man renderdaman.", data);
-
   boxEl.innerHTML = data
     .map((item) => {
       return `<div class="card">
-                <h1>${item.title}</h1>
-                <p>${item.description}</p>
+                <div>
+      <h3>${item.title}</h3>
+                <p>${item.description}</p></div>
+                <div>  
                 <button id="${item.id}_delete" class="delete_item">Delete</button>
-                <button id="${item.id}_edit" class="edit_item">Edit</button>
+              <button id="${item.id}_edit" class="edit_item">Edit</button>
+              </div>
               </div>`;
     })
     .join("");
 };
 const getData = () => {
-  console.log("get data chaqiruvi");
-
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log("Great! Man galdim~!");
-
       render(data);
     });
 };
 getData();
-console.log("Man formEla event qo'shaman.");
 
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -56,9 +52,8 @@ boxEl.addEventListener("click", (e) => {
   e.preventDefault();
   const el = e.target;
   const id = el.id.slice(0, el.id.indexOf("_"));
-
   if (el.className === "delete_item") {
-    boxEl.innerHTML = <p class="loading">LOADING...</p>;
+    boxEl.innerHTML = '<p class="loading">LOADING...</p>';
     fetch(`${url}/${el.id}`, {
       method: "DELETE",
     })
@@ -67,7 +62,8 @@ boxEl.addEventListener("click", (e) => {
         getData();
       });
   } else if (el.className === "edit_item") {
-    boxEl.innerHTML = <p class="loading">LOADING...</p>;
+    formEl.parentElement.parentElement.innerHTML = "";
+    boxEl.innerHTML = `<p class="loading">LOADING...</p>`;
     const form = document.createElement("form");
     form.className = "form_input";
     form.innerHTML = `
@@ -75,19 +71,19 @@ boxEl.addEventListener("click", (e) => {
                 class="inputs"
                 type="text"
                 name="title"
-                placeholder="Enter your change text"
-              />
+                placeholder="Enter new title"
+                required
+                />
               <input
                 class="inputs"
                 type="text"
-                name="descreption"
-                placeholder="Enter your change text"          
-              />
+                name="description"
+                placeholder="Enter new description"          
+                required  
+                />
             <button type="submit">Send</button>
     `;
     boxEl.innerHTML = "";
-    // boxEl.classList.add("active");
-    // boxEl.id = el.id;
 
     form.addEventListener("submit", (e) => {
       e.preventDefault();
@@ -95,7 +91,7 @@ boxEl.addEventListener("click", (e) => {
       console.log(inputs);
       const title = inputs[0].value;
       const description = inputs[1].value;
-      boxEl.innerHTML = <p class="loading">LOADING...</p>;
+      boxEl.innerHTML = '<p class="loading">LOADING...</p>';
       modal.innerHTML = "";
       modal.classList.toggle("hidden");
 
@@ -114,7 +110,6 @@ boxEl.addEventListener("click", (e) => {
         });
     });
 
-    // boxEl.appendChild(form);
     modal.appendChild(form);
     modal.classList.toggle("hidden");
   }
