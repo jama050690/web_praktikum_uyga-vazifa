a14445b5f3fe4d286be64a5040d99106a4799161
Jamshiddin <jbm050690@gmail.com>
2025-11-11 19:34:47 +0500

    js qismini yozdim

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251110/main.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251110/main.js
@@ -1,6 +1,24 @@
-const darkEl = document.querySelector(".dark_note");
-
+const darkEl = document.querySelector(".dark");
+const sun = document.querySelector("dark_note_1");
+const moon = document.querySelector("dark_note_2");
+let son = 0;
 darkEl.addEventListener("click", (e) => {
   e.preventDefault();
-  document.body.classList.toggle("dark_mode");
+  if (son === sun) {
+    document.body.classList.add("line_mode");
+    document.body.classList.remove("dark_mode");
+    moon.style.display = "none";
+    sun.style.display = "block";
+    moon.style.transition = "0.5s";
+    moon.hidden = true;
+    son = moon;
+  } else {
+    document.body.classList.add("dark_mode");
+    document.body.classList.remove("line_mode");
+    sun.style.display = "none";
+    moon.style.display = "block";
+    sun.style.transition = "0.5s";
+    sun.hidden = true;
+    son = sun;
+  }
 });
