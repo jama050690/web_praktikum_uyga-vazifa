c08832085cb9bcc40f3cc43c5e48c29659740815
Jamshiddin <jbm050690@gmail.com>
2025-11-12 17:28:08 +0500

    header qismini o'zgartirdim

--- Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251110/main.js
+++ Uyga_vazifalar_JSdan/uyga_vazifa_Jamshiddin_251110/main.js
@@ -1,24 +1,21 @@
 const darkEl = document.querySelector(".dark");
-const sun = document.querySelector("dark_note_1");
-const moon = document.querySelector("dark_note_2");
-let son = 0;
+const sun = document.querySelector(".dark_note_2");
+const moon = document.querySelector(".dark_note_1");
+let isDark = false;
+
 darkEl.addEventListener("click", (e) => {
   e.preventDefault();
-  if (son === sun) {
-    document.body.classList.add("line_mode");
-    document.body.classList.remove("dark_mode");
-    moon.style.display = "none";
-    sun.style.display = "block";
-    moon.style.transition = "0.5s";
-    moon.hidden = true;
-    son = moon;
-  } else {
+  isDark = !isDark;
+
+  if (isDark) {
     document.body.classList.add("dark_mode");
-    document.body.classList.remove("line_mode");
+    document.body.classList.remove("light_mode");
+    sun.style.display = "block";
+    moon.style.display = "none";
+  } else {
+    document.body.classList.add("light_mode");
+    document.body.classList.remove("dark_mode");
     sun.style.display = "none";
     moon.style.display = "block";
-    sun.style.transition = "0.5s";
-    sun.hidden = true;
-    son = sun;
   }
 });
