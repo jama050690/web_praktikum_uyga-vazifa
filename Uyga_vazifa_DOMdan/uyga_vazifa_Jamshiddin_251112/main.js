876cdd6d03ff9c2688ce3448ae57b59077ab57cd
Jamshiddin <jbm050690@gmail.com>
2025-11-12 20:10:35 +0500

    figmaga jsni uladim va banner qismini qildim

--- Uyga_vazifa_DOMdan/uyga_vazifa_Jamshiddin_251112/main.js
+++ Uyga_vazifa_DOMdan/uyga_vazifa_Jamshiddin_251112/main.js
@@ -1,6 +1,10 @@
 const darkEl = document.querySelector(".dark");
 const sun = document.querySelector(".dark_note_2");
 const moon = document.querySelector(".dark_note_1");
+const main_enter_content = document.querySelector(".main_enter_content");
+const baseUrl = "https://cars-project-six.vercel.app/";
+
+// Dark mode
 let isDark = false;
 
 darkEl.addEventListener("click", (e) => {
@@ -19,3 +23,28 @@ darkEl.addEventListener("click", (e) => {
     moon.style.display = "block";
   }
 });
+// Banner images change
+const bannerImages = (data) => {
+  main_enter_content.innerHTML = data
+    .map(
+      (item) => `
+    <div class="main_enter_text">
+      <h1 class="main_enter_title">${item.title}</h1>
+      <p class="main_enter_desc">${item.description}</p>
+    </div>
+    <img class="main_emage" src="${baseUrl}${data.image}" alt="Photo" />
+  `
+    )
+    .join("");
+};
+const bannerImageChange = async () => {
+  fetch(`${baseUrl}cars`)
+    .then((response) => response.json())
+    .then((data) => {
+      bannerImages(data[0]);
+    })
+    .catch((error) => {
+      console.error("Error fetching banner images:", error);
+    });
+};
+bannerImageChange();
