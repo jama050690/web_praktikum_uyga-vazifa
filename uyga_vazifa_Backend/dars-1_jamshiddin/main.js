ec3b0a345043009c0c40e3289e8ccc50a4b85f74
Jamshiddin <jbm050690@gmail.com>
2025-11-27 15:26:58 +0500

    masala 2 bajarildi

--- uyga_vazifa_Backend/dars-1_jamshiddin/main.js
+++ uyga_vazifa_Backend/dars-1_jamshiddin/main.js
@@ -13,3 +13,26 @@ class Talaba {
 
 const talaba = new Talaba("Jamshiddin", "Babajonov", 35, "Web praktikum:n4");
 console.log(talaba.info());
+
+class Culculator {
+  add(a, b) {
+    return a + b;
+  }
+  minus(a, b) {
+    return a - b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+  divided(a, b) {
+    if (b === 0) {
+      return "Soni 0 bo'lsa cheksizlik bo'ladi(infinity)";
+    }
+    return a / b;
+  }
+}
+const culculator = new Culculator();
+console.log(culculator.add(3, 6));
+console.log(culculator.minus(8, 6));
+console.log(culculator.multiply(3, 6));
+console.log(culculator.divided(6, 0));
