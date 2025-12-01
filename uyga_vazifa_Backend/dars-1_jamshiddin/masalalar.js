1a753842c4a23024b9e4946f8599e5eda1bbd5bc
Jamshiddin <jbm050690@gmail.com>
2025-12-01 16:55:32 +0500

    masalalar orasiga masalani ajratish uchun log berdim

--- uyga_vazifa_Backend/dars-1_jamshiddin/masalalar.js
+++ uyga_vazifa_Backend/dars-1_jamshiddin/masalalar.js
@@ -1,25 +1,36 @@
-// // // 1-masala
+console.log("Masala_1");
+// 1-masala
 var a;
 console.log(typeof a);
 var b = null;
 console.log(typeof b);
 console.log(typeof c);
 
-// // // 2-masala
+console.log("----------------------------");
+console.log("Masala_2");
+// 2-masala
 
-console.log(typeof ({} = null));
+// console.log(typeof ({} = null));
 
+console.log("----------------------------");
+console.log("Masala_3");
 // // 3-masala
 console.log(false + null / 3);
 
+console.log("----------------------------");
+console.log("Masala_4");
 // // 4-masal
 
 console.log([] + {} - undefined * true);
 
+console.log("----------------------------");
+console.log("Masala_5");
 // // 5-masala
 let m = [] + 3n;
 console.log(typeof b);
 
+console.log("----------------------------");
+console.log("Masala_6");
 // 6-masala
 
 let f = 5;
@@ -28,12 +39,19 @@ var d = 13.6;
 
 console.log(f / n == typeof d);
 
+console.log("----------------------------");
+console.log("Masala_7");
 // 7-masala
-console.log(([] = (true / null) * undefined - n));
+// console.log(([] = (true / null) * undefined - n));
 
+console.log("----------------------------");
+console.log("Masala_8");
 // 8-masala
 
 console.log(typeof isNaN(Boolean) / null);
+
+console.log("----------------------------");
+console.log("Masala_9");
 //  masala-9
 
 let h = "hi";
@@ -41,27 +59,40 @@ var b = null;
 const i = {};
 console.log(typeof b - i * h);
 
-// 9-masala
+console.log("----------------------------");
+console.log("Masala_10");
+// 10-masala
 console.log(typeof n == {} && isNaN == []);
 
-// 10-masala
-
+console.log("----------------------------");
+console.log("Masala_11");
+// 11-masala
 console.log([] + (2 / a) * true ? "bravo" : 0);
 
-// 11-masala
+console.log("----------------------------");
+console.log("Masala_12");
+// 12-masala
 var k = !([] + {}) * null == 1;
 console.log(k);
 
-// 12-masala
+console.log("----------------------------");
+console.log("Masala_13");
+// 13-masala
 console.log(typeof (3n == "NaN") / undefined);
 
-// 13-masala
+console.log("----------------------------");
+console.log("Masala_14");
+// 14-masala
 console.log((let = a % 3 == -1));
 
-// 14-masala
-console.log(typeof [string](fan) - -1);
-
+console.log("----------------------------");
+console.log("Masala_15");
 // 15-masala
+// console.log(typeof [string](fan) - -1);
+
+console.log("----------------------------");
+console.log("Masala_16");
+// 16-masala
 
 var j = 45;
 console.log(typeof j[{}]);
