d82290ed670dfe4e7ba093402c17345ceafae03d
Jamshiddin <jbm050690@gmail.com>
2025-12-26 19:45:13 +0500

    projectni css qismi qoldi

--- uyga_vazifa_Backend/uyga vazifa_13_Jamshiddin/project_Talaba/server.js
+++ uyga_vazifa_Backend/uyga vazifa_13_Jamshiddin/project_Talaba/server.js
@@ -1,7 +1,45 @@
-import { promises as fs } from "fs";
 import express from "express";
-import ejs from "ejs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.listen(3000, () => console.log("Server running on 3000"));
+// EJS (Express 5 avtomatik)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// body parser
+app.use(express.urlencoded({ extended: true }));
+
+// static
+app.use("/css", express.static(path.join(__dirname, "css")));
+
+// vaqtinchalik DB
+const users = [];
+
+// LIST + FORM
+app.get("/talabalar", (req, res) => {
+  res.render("talabalar", { users });
+});
+
+// CREATE
+app.post("/talabalar", (req, res) => {
+  const { name } = req.body;
+
+  if (name?.trim()) {
+    users.push({
+      id: Date.now(),
+      name: name.trim(),
+      age: Number(age),
+    });
+  }
+
+  res.redirect("/talabalar");
+});
+
+app.listen(3000, () => {
+  console.log("http://127.0.0.1:3000/talabalar");
+});
