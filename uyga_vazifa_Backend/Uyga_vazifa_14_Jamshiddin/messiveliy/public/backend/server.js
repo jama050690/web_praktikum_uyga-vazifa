092a79256c0d17cf52560b2655e5c5f2093b0e65
Jamshiddin <jbm050690@gmail.com>
2025-12-30 18:34:54 +0500

    linklarni ulab chiqdim

--- uyga_vazifa_Backend/Uyga_vazifa_14_Jamshiddin/messiveliy/public/backend/server.js
+++ uyga_vazifa_Backend/Uyga_vazifa_14_Jamshiddin/messiveliy/public/backend/server.js
@@ -1,40 +1,85 @@
 import express from "express";
 import { promises as fs } from "fs";
 import ejs from "ejs";
+
 const PORT = 3000;
 const app = express();
+
+//  DATA
 const images = [
   {
     image: {
-      full: "images/(01).jpg",
-      thumb: "images/(02).jpg",
+      full: "/images/01.jpg",
+      thumb: "/images/02.jpg",
     },
     name: "Diam compus accumsan",
     description: "lorem in contact is not problem",
   },
 ];
 
-app.use("/images", express.static("public/images"));
-app.use("/assets", express.static("public/assets"));
-app.get("/", async (req, res) => {
-  const home = await fs.readFile("./view/index.html", "utf8");
-  const home_1 = await fs.readFile("./view/elements.html", "utf8");
-  const home_2 = await fs.readFile("./view/generic.html", "utf8");
-  const result = ejs.render(home, home_1, home_2, {
+//  STATIC
+app.use("/images", express.static("images"));
+app.use("/assets", express.static("assets"));
+
+//  ROUTES
+
+// HOME
+app.get("/index.html", async (req, res) => {
+  const home = await fs.readFile("view/index.html", "utf8");
+
+  const result = ejs.render(home, {
     title: "Welcome to our website!",
     projectName: "My Gallery",
-    descreption: "The next level image gallery",
+    description: "The next level image gallery",
     telegramUsername: "Jama_9133",
-    whatsapUsername: 998957990034,
-    emailUsername: "jbm050690@gmail.com",
+    whatsappUsername: "998957990034",
     instagramUsername: "jamshiddinbabajonov",
     linkedinUsername: "jamshiddin-babajonov-168705382",
     githupUsername: "jama050690",
+    emailUsername: "jbm050690@gmail.com",
+    images,
+  });
+
+  res.send(result);
+});
+
+// ELEMENTS
+app.get("/elements.html", async (req, res) => {
+  const elements = await fs.readFile("view/elements.html", "utf8");
+  const result = ejs.render(elements, {
+    title: "Elements",
+    projectName: "My Gallery",
+    description: "The next level image gallery",
+    telegramUsername: "Jama_9133",
+    whatsappUsername: "998957990034",
+    instagramUsername: "jamshiddinbabajonov",
+    linkedinUsername: "jamshiddin-babajonov-168705382",
+    githupUsername: "jama050690",
+    emailUsername: "jbm050690@gmail.com",
     images,
   });
   res.send(result);
 });
 
+// GENERIC
+app.get("/generic.html", async (req, res) => {
+  const generic = await fs.readFile("view/generic.html", "utf8");
+  const result = ejs.render(generic, {
+    title: "Generic",
+    projectName: "My Gallery",
+    description: "The next level image gallery",
+    telegramUsername: "Jama_9133",
+    whatsappUsername: "998957990034",
+    instagramUsername: "jamshiddinbabajonov",
+    linkedinUsername: "jamshiddin-babajonov-168705382",
+    githupUsername: "jama050690",
+    emailUsername: "jbm050690@gmail.com",
+    images,
+  });
+  res.send(result);
+});
+
+// SERVER
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(` Server running on http://localhost:${PORT}`);
 });
