9157c7234f0503cb8863ea812ad20737aefd0194
Jamshiddin <jbm050690@gmail.com>
2026-01-01 12:49:26 +0500

    loyighani o'qitib oldik

--- uyga_vazifa_Backend/Uyga_vazifa_14_Jamshiddin/prolugue/public/backend/server.js
+++ uyga_vazifa_Backend/Uyga_vazifa_14_Jamshiddin/prolugue/public/backend/server.js
@@ -1,38 +1,58 @@
 import express from "express";
-import { promises as fs } from "fs";
+import path from "path";
+import fs from "fs/promises";
 import ejs from "ejs";
+import { fileURLToPath } from "url";
+
 const PORT = 3000;
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// static
+app.use("/images", express.static(path.join(__dirname, "../images")));
+app.use("/assets", express.static(path.join(__dirname, "../assets")));
+
+// DATA
 const images = [
   {
     image: {
-      full: "images/full/01.jbg",
-      thumb: "images/thumb/01.jbg",
+      full: "images/full/01.jpg",
+      thumb: "images/thumb/01.jpg",
     },
-    name: "Diam compus accumsan",
-    descreption: "lorem in contact is not problem",
+    name: "This my photos",
+    description: "lorem in contact is not problem",
   },
 ];
 
-app.use(`/images`, express.static(`public/images`));
-app.use(`/assets`, express.static(`public/assets`));
+const navItems = [
+  { href: "#top", icon: "fa-home", text: "Intro" },
+  { href: "#about", icon: "fa-user", text: "About" },
+  { href: "#contact", icon: "fa-envelope", text: "Contact" },
+];
+
+// ROUTE
 app.get("/index.html", async (req, res) => {
-  const home = await fs.readFile("./public/index.html", "utf8");
-  const result = ejs.render(home, {
+  const html = await fs.readFile(path.join(__dirname, "../index.html"), "utf8");
+
+  const rendered = ejs.render(html, {
     title: "Welcome to our website!",
-    projectName: "My Gallery",
-    descreption: "The next level image gallery",
+    projectName: "This is my website",
+    description: "The next level my images gallery",
     telegramUsername: "Jama_9133",
-    whatsapUsername: 998957990034,
+    whatsapUsername: "998957990034",
     emailUsername: "jbm050690@gmail.com",
     instagramUsername: "jamshiddinbabajonov",
     linkedinUsername: "jamshiddin-babajonov-168705382",
-    githupUsername: "jama050690",
+    githubUsername: "jama050690",
     images,
+    navItems,
   });
-  res.send(result);
+
+  res.send(rendered);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(` Server running on http://localhost:${PORT}`);
 });
