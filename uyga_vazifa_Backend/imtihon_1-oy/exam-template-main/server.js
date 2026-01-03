2bbf6f9204572fcc047ef656d901c837b247b7d8
Jamshiddin <jbm050690@gmail.com>
2026-01-03 22:52:17 +0500

    imtihon vazifani bajardim ejs orqali

--- uyga_vazifa_Backend/imtihon_1-oy/exam-template-main/server.js
+++ uyga_vazifa_Backend/imtihon_1-oy/exam-template-main/server.js
@@ -7,7 +7,9 @@ const PORT = 3000;
 
 //  STATIC
 app.use("/img", express.static("img"));
-app.use("/public", express.static("public"));
+app.use("/css", express.static("css"));
+app.use("/js", express.static("js"));
+app.use("/lib", express.static("lib"));
 
 // DATA
 const tableData = [
@@ -29,38 +31,65 @@ const tableData = [
 
 //  PAGES
 app.get("/signin", async (req, res) => {
-  const html = await fs.readFile("./signin.html", "utf8");
+  const view = await fs.readFile("./index.html", "utf8");
+  const html = ejs.render(view, {
+    projectName: "My Project",
+    userName: "Jamshiddin",
+  });
   res.send(html);
 });
 
 app.get("/signup", async (req, res) => {
-  const html = await fs.readFile("./signup.html", "utf8");
+  const view = await fs.readFile("./index.html", "utf8");
+  const html = ejs.render(view, {
+    projectName: "My Project",
+    userName: "Jamshiddin",
+  });
   res.send(html);
 });
 
 app.get("/404", async (req, res) => {
-  const html = await fs.readFile("./404.html", "utf8");
+  const view = await fs.readFile("./index.html", "utf8");
+  const html = ejs.render(view, {
+    projectName: "My Project",
+    userName: "Jamshiddin",
+  });
   res.send(html);
 });
-
 app.get("/chart", async (req, res) => {
-  const html = await fs.readFile("./chart.html", "utf8");
+  const view = await fs.readFile("./index.html", "utf8");
+  const html = ejs.render(view, {
+    projectName: "My Project",
+    userName: "Jamshiddin",
+  });
   res.send(html);
 });
 
 app.get("/blank", async (req, res) => {
-  const html = await fs.readFile("./blank.html", "utf8");
+  const view = await fs.readFile("./index.html", "utf8");
+  const html = ejs.render(view, {
+    projectName: "My Project",
+    userName: "Jamshiddin",
+  });
   res.send(html);
 });
 
 app.get("/widget", async (req, res) => {
-  const html = await fs.readFile("./widget.html", "utf8");
+  const view = await fs.readFile("./index.html", "utf8");
+  const html = ejs.render(view, {
+    projectName: "My Project",
+    userName: "Jamshiddin",
+  });
   res.send(html);
 });
 
 app.get("/table", async (req, res) => {
   const view = await fs.readFile("./table.html", "utf8");
-  const html = ejs.render(view, { table: tableData });
+  const html = ejs.render(view, {
+    projectName: "My Project",
+    userName: "Jamshiddin",
+    table: tableData,
+  });
   res.send(html);
 });
 
@@ -69,7 +98,6 @@ app.get("/index", async (req, res) => {
   const html = ejs.render(view, {
     projectName: "My Project",
     userName: "Jamshiddin",
-    table: tableData,
   });
   res.send(html);
 });
