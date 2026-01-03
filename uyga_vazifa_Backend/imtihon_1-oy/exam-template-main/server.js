531be66f1610879951231b2311f66a9e0810f435
Jamshiddin <jbm050690@gmail.com>
2026-01-03 20:42:47 +0500

    imtihon vazifasini bajardim ejs orqali

---
+++ uyga_vazifa_Backend/imtihon_1-oy/exam-template-main/server.js
@@ -0,0 +1,79 @@
+import express from "express";
+import ejs from "ejs";
+import { promises as fs } from "fs";
+
+const app = express();
+const PORT = 3000;
+
+//  STATIC
+app.use("/img", express.static("img"));
+app.use("/public", express.static("public"));
+
+// DATA
+const tableData = [
+  { scope: "#", first: "First Name", last: "Last Name", email: "Email" },
+  {
+    scope: 1,
+    first: "Jamshiddin",
+    last: "Babajonov",
+    email: "jbm050690@mail.com",
+  },
+  { scope: 2, first: "Tolmas", last: "O'rinov", email: "o'rinov_007@mail.com" },
+  {
+    scope: 3,
+    first: "Sardor",
+    last: "Sunnatullayev",
+    email: "sador_05_02@mail.com",
+  },
+];
+
+//  PAGES
+app.get("/signin", async (req, res) => {
+  const html = await fs.readFile("./signin.html", "utf8");
+  res.send(html);
+});
+
+app.get("/signup", async (req, res) => {
+  const html = await fs.readFile("./signup.html", "utf8");
+  res.send(html);
+});
+
+app.get("/404", async (req, res) => {
+  const html = await fs.readFile("./404.html", "utf8");
+  res.send(html);
+});
+
+app.get("/chart", async (req, res) => {
+  const html = await fs.readFile("./chart.html", "utf8");
+  res.send(html);
+});
+
+app.get("/blank", async (req, res) => {
+  const html = await fs.readFile("./blank.html", "utf8");
+  res.send(html);
+});
+
+app.get("/widget", async (req, res) => {
+  const html = await fs.readFile("./widget.html", "utf8");
+  res.send(html);
+});
+
+app.get("/table", async (req, res) => {
+  const view = await fs.readFile("./table.html", "utf8");
+  const html = ejs.render(view, { table: tableData });
+  res.send(html);
+});
+
+app.get("/index", async (req, res) => {
+  const view = await fs.readFile("./index.html", "utf8");
+  const html = ejs.render(view, {
+    projectName: "My Project",
+    userName: "Jamshiddin",
+    table: tableData,
+  });
+  res.send(html);
+});
+
+app.listen(PORT, () => {
+  console.log(` Server running on http://localhost:${PORT}`);
+});
